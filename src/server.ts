fa3c28c001b52ae952f7377f02a71af30fd892b2
t.emidio <t_emidio@hotmail.com>
2021-06-23 18:29:57 -0300

    third class

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@
 import "reflect-metadata";
 import "./database";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
-
+app.use(express.json());
+app.use(router);
 app.listen(3000, ()=> console.log("Server is now running NLW"));
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserService {
+    name: string,
+    email: string,
+    admin: boolean,
+
+}
+
+class CreateUserService{
+    async execute ({name, email, admin}:IUserService){
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        if (!email){
+            throw new Error("Incorrect Email");
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({ email
+        });
+        
+        if (userAlreadyExists){
+            throw new Error("User Already Exists");
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        })
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+
+}
+
+export { CreateUserService };
